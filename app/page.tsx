ef3f1fd26d4ad2fa8bc86a989befb2c123d9ad4a
Rafał Goławski <rafal.j.golawski@gmail.com>
2024-10-19 00:35:01 +0200

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,70 @@
-import Image from "next/image";
+import { GithubIcon, LinkedinIcon, LinkIcon, MailIcon, TwitterIcon } from 'lucide-react'
+
+const iconMap = {
+  github: GithubIcon,
+  linkedin: LinkedinIcon,
+  twitter: TwitterIcon,
+  mail: MailIcon,
+  other: LinkIcon,
+}
+
+function LinkButton({
+  variant,
+  href,
+  label,
+}: {
+  variant: 'github' | 'linkedin' | 'twitter' | 'mail' | 'other'
+  href: string
+  label: string
+}) {
+  const Icon = iconMap[variant]
+  const linkProps = variant !== 'mail' ? { target: '_blank', rel: 'noopener noreferrer' } : {}
+  return (
+    <a
+      className="inline-flex items-center justify-center space-x-1.5 rounded-lg border border-neutral-700 bg-gradient-to-b from-white/15 px-3 py-2 text-xs font-semibold text-white shadow-md shadow-white/10 transition-all hover:-translate-y-[2px] hover:border-neutral-500"
+      href={href}
+      {...linkProps}
+    >
+      <Icon size={16} />
+      <span>{label}</span>
+    </a>
+  )
+}
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <main className="flex min-h-screen items-center justify-center bg-neutral-950 p-5 font-mono">
+      <div className="max-w-xl text-balance text-center lowercase text-white">
+        <h1 className="text-2xl font-bold">Rafal Golawski</h1>
+        <p className="mt-2 text-sm leading-relaxed text-neutral-100">
+          TLDR: Front-end developer with over 6 years of experience. Lifting weights four times a
+          week, zone 2 running once a week, coding at 9-5. Simplicity over complexity.
+        </p>
+        <div className="mb-7 mt-5 flex items-center space-x-4">
+          <div className="relative h-[2px] w-full rounded-full bg-pink-500">
+            <div className="animate-flicker absolute -inset-0.5 bg-gradient-to-r from-fuchsia-500 to-pink-500 blur" />
+          </div>
+          <p className="whitespace-nowrap text-sm text-neutral-100">Reach out</p>
+          <div className="relative h-[2px] w-full rounded-full bg-pink-500">
+            <div className="animate-flicker absolute -inset-0.5 bg-gradient-to-r from-pink-500 to-fuchsia-500 blur" />
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+        <div className="flex flex-col flex-wrap justify-center gap-2 md:flex-row">
+          <LinkButton variant="github" href="https://github.com/rago4" label="rago4" />
+          <LinkButton
+            variant="linkedin"
+            href="https://linkedin.com/in/rgolawski"
+            label="rgolawski"
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
+          <LinkButton variant="twitter" href="https://twitter.com/rgolawski" label="rgolawski" />
+          <LinkButton variant="other" href="https://dev.to/rgolawski" label="dev.to/rgolawski" />
+          <LinkButton
+            variant="mail"
+            href="mailto:rafal.j.golawski@gmail.com"
+            label="rafal.j.golawski@gmail.com"
           />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+        </div>
+      </div>
+    </main>
+  )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,30 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      animation: {
+        flicker: 'flicker 5s ease infinite',
+      },
+      keyframes: {
+        flicker: {
+          '0%, 19.999%, 22%, 62.999%, 64%, 64.999%, 70%, 100%': {
+            opacity: '0.99',
+            filter: 'brightness(1) saturate(1) blur(8px)',
+          },
+          '20%, 21.999%, 63%, 63.999%, 65%, 69.999%': {
+            opacity: '0.4',
+            filter: 'brightness(0.8) saturate(0.8) blur(8px)',
+          },
+        },
       },
     },
   },
   plugins: [],
-};
-export default config;
+}
+export default config
