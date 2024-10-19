0c9c61ee91e2f10314183972780ef2e4d9e0dc0a
Rafał Goławski <rafal.j.golawski@gmail.com>
2024-10-20 01:33:55 +0200

    init cv-builder

---
+++ app/cv-builder/page.tsx
@@ -0,0 +1,9 @@
+import { MainContent } from '@/cv-builder/components/main-content'
+
+export default function CVBuilderPage() {
+  return (
+    <main className="grid min-h-screen text-slate-900 md:grid-cols-2">
+      <MainContent />
+    </main>
+  )
+}
