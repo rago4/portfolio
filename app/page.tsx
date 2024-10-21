fd7c78c40ff255c8e379ef8883282d300eace349
Rafał Goławski <rafal.j.golawski@gmail.com>
2024-10-21 23:33:33 +0200

    add link to the resume builder to the main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,47 @@
-import { GithubIcon, LinkedinIcon, LinkIcon, MailIcon, TwitterIcon } from 'lucide-react'
+import {
+  FileUserIcon,
+  GithubIcon,
+  LinkedinIcon,
+  LinkIcon,
+  MailIcon,
+  TwitterIcon,
+} from 'lucide-react'
+import Link from 'next/link'
 
 const iconMap = {
   github: GithubIcon,
   linkedin: LinkedinIcon,
-  twitter: TwitterIcon,
   mail: MailIcon,
   other: LinkIcon,
+  resume: FileUserIcon,
+  twitter: TwitterIcon,
 }
+
+const sectionStyles = 'flex flex-col flex-wrap justify-center gap-2 md:flex-row'
 
 function LinkButton({
   variant,
   href,
   label,
 }: {
-  variant: 'github' | 'linkedin' | 'twitter' | 'mail' | 'other'
+  variant: keyof typeof iconMap
   href: string
   label: string
 }) {
   const Icon = iconMap[variant]
-  const linkProps = variant !== 'mail' ? { target: '_blank', rel: 'noopener noreferrer' } : {}
+  const internal = href.startsWith('/')
+  const LinkComponent = internal ? Link : 'a'
+  const linkProps =
+    !internal && variant !== 'mail' ? { target: '_blank', rel: 'noopener noreferrer' } : {}
   return (
-    <a
+    <LinkComponent
       className="inline-flex items-center justify-center space-x-1.5 rounded-lg border border-neutral-700 bg-gradient-to-b from-white/15 px-3 py-2 text-xs font-semibold text-white shadow-md shadow-white/10 transition-all hover:-translate-y-[2px] hover:border-neutral-500"
       href={href}
       {...linkProps}
     >
       <Icon size={16} />
       <span>{label}</span>
-    </a>
+    </LinkComponent>
   )
 }
 
@@ -40,7 +54,7 @@ export default function Home() {
           TLDR: Front-end developer with over 6 years of experience. Lifting weights three times a
           week, zone 2 running once a week, coding at 9-5. Simplicity over complexity.
         </p>
-        <div className="mb-7 mt-5 flex items-center space-x-4">
+        <div className="mb-6 mt-5 flex items-center space-x-4">
           <div className="relative h-[2px] w-full rounded-full bg-pink-500">
             <div className="absolute -inset-0.5 animate-flicker bg-gradient-to-r from-fuchsia-500 to-pink-500 blur" />
           </div>
@@ -49,7 +63,7 @@ export default function Home() {
             <div className="absolute -inset-0.5 animate-flicker bg-gradient-to-r from-pink-500 to-fuchsia-500 blur" />
           </div>
         </div>
-        <div className="flex flex-col flex-wrap justify-center gap-2 md:flex-row">
+        <section className={sectionStyles}>
           <LinkButton variant="github" href="https://github.com/rago4" label="rago4" />
           <LinkButton
             variant="linkedin"
@@ -63,7 +77,11 @@ export default function Home() {
             href="mailto:rafal.j.golawski@gmail.com"
             label="rafal.j.golawski@gmail.com"
           />
-        </div>
+        </section>
+        <p className="my-6 text-sm text-neutral-100">Or check out my recent projects</p>
+        <section className={sectionStyles}>
+          <LinkButton variant="resume" href="/resume-builder" label="Resume Builder" />
+        </section>
       </div>
     </main>
   )
