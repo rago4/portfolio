de1dff2d59d3e07c6a45a9b82fc6f6fba0ccb5bf
Rafał Goławski <rafal.j.golawski@gmail.com>
2024-10-29 00:12:56 +0100

    dev-analytics: replace context with state

--- dev-analytics/components/articles-menu.tsx
+++ dev-analytics/components/articles-menu.tsx
@@ -1,31 +1,33 @@
-'use client'
-
 import { Spinner } from '@/dev-analytics/components/ui/spinner'
-import { useMainContext } from '@/dev-analytics/main-context'
 import { useArticles } from '@/dev-analytics/services'
 import { cn } from '@/dev-analytics/utils'
 
-export function ArticlesMenu() {
-  const { data, isLoading, error } = useArticles()
-  const { currentArticle, setCurrentArticle } = useMainContext()
-  if (isLoading) {
+export function ArticlesMenu({
+  activeId,
+  onItemClick,
+}: {
+  activeId: string
+  onItemClick: (id: string) => void
+}) {
+  const articles = useArticles()
+  if (articles.isLoading) {
     return <Spinner className="mx-auto" />
   }
-  if (error) {
+  if (articles.error) {
     return <p className="text-sm text-red-600">Failed to load articles</p>
   }
   return (
     <ul className="space-y-2">
-      {data?.data.map((article) => (
+      {articles.data?.data.map((article) => (
         <li key={article.id}>
           <button
             className={cn(
               'w-full truncate rounded px-3 py-2 text-left text-sm font-medium hover:bg-slate-100',
-              currentArticle?.id === article.id ? 'bg-slate-100' : 'bg-white'
+              article.id === activeId ? 'bg-slate-100' : 'bg-white'
             )}
             type="button"
             onClick={() => {
-              setCurrentArticle(article)
+              onItemClick(article.id)
             }}
           >
             {article.title}
--- dev-analytics/components/main-content.tsx
+++ dev-analytics/components/main-content.tsx
@@ -1,21 +1,34 @@
 'use client'
 
+import { useState } from 'react'
+
 import { ArticleHeader } from '@/dev-analytics/components/article-header'
 import { ArticlesMenu } from '@/dev-analytics/components/articles-menu'
-import { MainContextProvider } from '@/dev-analytics/main-context'
+import { type Article, useArticles } from '@/dev-analytics/services'
 
 export function MainContent() {
+  const articles = useArticles()
+  const [article, setArticle] = useState<Article | undefined>(undefined)
   return (
-    <MainContextProvider>
-      <div className="grid h-[calc(100dvh-63px)] grid-cols-12">
-        <aside className="col-span-3 space-y-4 overflow-y-auto border-r border-slate-300 p-4">
-          <h2 className="text-lg font-bold">Recent Posts</h2>
-          <ArticlesMenu />
-        </aside>
-        <main className="col-span-9 overflow-y-auto bg-slate-50 p-8">
-          <ArticleHeader />
-        </main>
-      </div>
-    </MainContextProvider>
+    <div className="grid h-[calc(100dvh-63px)] grid-cols-12">
+      <aside className="col-span-3 space-y-4 overflow-y-auto border-r border-slate-300 p-4">
+        <h2 className="text-lg font-bold">Recent Posts</h2>
+        <ArticlesMenu
+          activeId={article?.id ?? ''}
+          onItemClick={(id) => {
+            setArticle(articles.data?.data.find((article) => article.id === id))
+          }}
+        />
+      </aside>
+      <main className="col-span-9 overflow-y-auto bg-slate-50 p-8">
+        {!article ? (
+          <div className="flex h-full w-full items-center justify-center">
+            <p className="text-sm text-slate-600">Select an article to see more details</p>
+          </div>
+        ) : (
+          <ArticleHeader data={article} />
+        )}
+      </main>
+    </div>
   )
 }
